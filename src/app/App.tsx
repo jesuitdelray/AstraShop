8e7249489947361eb890674ff0d2daa8b64ffce8
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-15 10:31:56 +0200

    cleanup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,12 @@
-import { Suspense, useState } from "react"
+import { Suspense } from "react"
 import { classNames } from "shared/lib/classNames/classNames"
 import { AppRouter } from "app/providers/router"
-import { Modal } from "shared/ui/Modal/Modal"
 
 function App() {
-    const [toggle, setToggle] = useState(false)
     return (
         <div className={classNames("app", {}, [])}>
             <Suspense fallback="">
                 Navigation
-                <button onClick={() => setToggle(true)}>toggle</button>
-                <Modal isOpen={toggle} onClose={() => setToggle(false)}>
-                    Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-                </Modal>
                 <div className="content-page">
                     <AppRouter />
                 </div>
