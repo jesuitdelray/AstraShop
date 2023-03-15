cad52d378f13ff07a881f8aa920ba6e71472a1b0
VadimGalian <vadim.galian@gmail.com>
2023-03-15 15:08:57 +0200

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,34 +1,12 @@
-import { Suspense, useState } from "react"
+import { Suspense } from "react"
 import { classNames } from "shared/lib/classNames/classNames"
 import { AppRouter } from "app/providers/router"
 
-import { Input, InputType } from "shared/ui/Input/Input"
-
 function App() {
-    const [inputs, setInputs] = useState("")
-
     return (
         <div className={classNames("app", {}, [])}>
             <Suspense fallback="">
                 Navigation
-                <Input
-                    type={InputType.PASSWORD}
-                    placeholder="Enter your name"
-                    label="Имя, Фамилия"
-                    value={inputs}
-                    onChange={(value: string) => setInputs(value)}
-                    error="Please Enter Correct Name!!!"
-                    id="123"
-                />
-                <Input
-                    type={InputType.PASSWORD}
-                    placeholder="Enter your name"
-                    label="Имя, Фамилия"
-                    value={inputs}
-                    onChange={(value: string) => setInputs(value)}
-                    error="Please Enter Correct Name!!!"
-                    id="124"
-                />
                 <div className="content-page">
                     <AppRouter />
                 </div>
