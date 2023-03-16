91f9225c30744529c89dfe601422924dc7253570
VadimGalian <vadim.galian@gmail.com>
2023-03-16 18:11:23 +0200

    update

--- src/pages/TestPage/ui/TestPage.tsx
+++ src/pages/TestPage/ui/TestPage.tsx
@@ -1,7 +1,5 @@
-import { ShopBagIcon } from "shared/assets/icons/list"
-import { ShoppingBagIcon } from "shared/assets/icons/others"
 import styles from "./TestPage.module.scss"
 
 export function TestPage() {
-    return <div>TEST</div>
+    return <div>TestPage</div>
 }
