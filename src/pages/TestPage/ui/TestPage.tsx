4f5dfbe9239652de5093a2d6c63c090ecd8c1a3e
VadimGalian <vadim.galian@gmail.com>
2023-03-16 18:00:24 +0200

    updated svg

--- src/pages/TestPage/ui/TestPage.tsx
+++ src/pages/TestPage/ui/TestPage.tsx
@@ -1,1 +1,7 @@
-export function TestPage() {}
+import { ShopBagIcon } from "shared/assets/icons/list"
+import { ShoppingBagIcon } from "shared/assets/icons/others"
+import styles from "./TestPage.module.scss"
+
+export function TestPage() {
+    return <div>TEST</div>
+}
--- src/shared/assets/icons/others/index.ts
+++ src/shared/assets/icons/others/index.ts
@@ -1,24 +1,17 @@
 import FacebookIcon from "./facebook.svg"
-import ShoppingBagRedIcon from "./shopping-bag-red.svg"
+import ShoppingBagIcon from "./shopping-bag.svg"
 import MenuIcon from "./menu.svg"
 import SearchIcon from "./search.svg"
 import DeleteBucketIcon from "./delete-bucket.svg"
 import PlusIcon from "./plus.svg"
 import MinusIcon from "./minus.svg"
 
-import {
-    ShoppingBagIcon,
-    ShoppingBagIconColor,
-} from "./ShoppingBagIcon/ShoppingBagIcon"
-
 export {
     MinusIcon,
     PlusIcon,
     DeleteBucketIcon,
     FacebookIcon,
-    ShoppingBagRedIcon,
     MenuIcon,
     SearchIcon,
     ShoppingBagIcon,
-    ShoppingBagIconColor,
 }
