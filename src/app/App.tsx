8a2bfa871340d7db4a1dba63b2575fc42ca22859
VadimGalian <vadim.galian@gmail.com>
2023-03-16 15:27:40 +0200

    create label component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,21 @@
 import { Suspense } from "react"
 import { classNames } from "shared/lib/classNames/classNames"
 import { AppRouter } from "app/providers/router"
+import { Label, LabelColor, LabelFontSize } from "shared/ui/Label/Label"
 
 function App() {
     return (
         <div className={classNames("app", {}, [])}>
             <Suspense fallback="">
-                Navigation
+                {/*  Navigation
                 <div className="content-page">
                     <AppRouter />
-                </div>
+                </div> */}
+                <Label
+                    value="0"
+                    fontSize={LabelFontSize.FONT_SMALL}
+                    color={LabelColor.GRAY}
+                />
             </Suspense>
         </div>
     )
