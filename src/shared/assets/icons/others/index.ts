3b6d119fdfaff75ecaab4e80cf01d4e4e8935572
je-delray <hovax74935@ekcsoft.com>
2023-03-16 16:31:15 +0100

    4th

--- src/shared/assets/icons/others/index.ts
+++ src/shared/assets/icons/others/index.ts
@@ -3,6 +3,8 @@ import ShoppingBagRedIcon from "./shopping-bag-red.svg"
 import MenuIcon from "./menu.svg"
 import SearchIcon from "./search.svg"
 import DeleteBucketIcon from "./delete-bucket.svg"
+import PlusIcon from "./plus.svg"
+import MinusIcon from "./minus.svg"
 
 import {
     ShoppingBagIcon,
@@ -10,6 +12,8 @@ import {
 } from "./ShoppingBagIcon/ShoppingBagIcon"
 
 export {
+    MinusIcon,
+    PlusIcon,
     DeleteBucketIcon,
     FacebookIcon,
     ShoppingBagRedIcon,
