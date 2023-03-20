aaef75e0b8de6b3c637ef9d4fec9a3208c1900d2
je-delray <hovax74935@ekcsoft.com>
2023-03-20 10:52:34 +0100

    1st

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,3 +1,14 @@
+import styles from "./Footer.module.scss"
+
 export function Footer() {
-    return <div>Footer</div>
+    return (
+        <div className={styles.footer}>
+            <div className={styles.commerceContainer}>1</div>
+            <div className={styles.infoContainer}>
+                <div>1111</div>
+                <div>5555</div>
+                <div>9999</div>
+            </div>
+        </div>
+    )
 }
