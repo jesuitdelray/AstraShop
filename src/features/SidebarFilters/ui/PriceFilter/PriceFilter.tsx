ae80787f43319f3e80f34b390d2d53a8f7b4124a
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-22 11:54:56 +0200

    upd

--- src/features/SidebarFilters/ui/PriceFilter/PriceFilter.tsx
+++ src/features/SidebarFilters/ui/PriceFilter/PriceFilter.tsx
@@ -4,11 +4,15 @@ import { RangeSlider } from "features/SidebarFilters/ui/PriceFilter/RangeSlider/
 import { Typography, TypographyColor, TypographyVariant } from "shared/ui/Typography/Typography"
 import styles from "./PriceFilter.module.scss"
 
-const TOTAL = 10000
+const MINIMUM_PRICE = 1000
+const MAXIMUM_PRICE = 10000
 const GAP = 1000
 
 export function PriceFilter() {
-    const [priceSort, setPriceSort] = useState({ min: 0, max: TOTAL / 2 })
+    const [priceSort, setPriceSort] = useState({
+        min: MINIMUM_PRICE,
+        max: (MAXIMUM_PRICE - MINIMUM_PRICE) / 2 + MINIMUM_PRICE,
+    })
     const [position, setPosition] = useState({
         left: "0%",
         right: "50%",
@@ -22,6 +26,7 @@ export function PriceFilter() {
             minRef.current.value = priceSort.min.toString()
             maxRef.current.value = priceSort.max.toString()
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     function changeHandler(value: string, side: string) {
@@ -29,14 +34,24 @@ export function PriceFilter() {
             return
         }
 
+        const { min, max } = priceSort
+
         if (side === "min") {
             if (priceSort.max - +value < GAP) {
                 setPriceSort(prev => ({ ...prev, min: prev.max - GAP }))
                 if (minRef.current) minRef.current.value = (priceSort.max - GAP).toString()
-                const left = `${((+priceSort.max - GAP) / TOTAL) * 100}%`
+                const left = `${
+                    ((+priceSort.max - GAP - MINIMUM_PRICE) / (MAXIMUM_PRICE - MINIMUM_PRICE)) * 100
+                }%`
                 setPosition(prev => ({ ...prev, left }))
+            } else if (+value < MINIMUM_PRICE) {
+                setPriceSort(prev => ({ ...prev, min: +value }))
+                if (minRef.current) minRef.current.value = MINIMUM_PRICE.toString()
+                setPosition(prev => ({ ...prev, left: "0%" }))
             } else {
-                const left = `${(+value / TOTAL) * 100}%`
+                const left = `${
+                    ((+value - MINIMUM_PRICE) / (MAXIMUM_PRICE - MINIMUM_PRICE)) * 100
+                }%`
                 setPosition(prev => ({ ...prev, left }))
                 setPriceSort(prev => ({ ...prev, min: +value }))
                 if (minRef.current) minRef.current.value = value
@@ -47,14 +62,19 @@ export function PriceFilter() {
                 if (maxRef.current) {
                     maxRef.current.value = (priceSort.min + GAP).toString()
                 }
-                const right = `${100 - ((priceSort.min + GAP) / TOTAL) * 100}%`
+                const right = `${
+                    100 -
+                    ((priceSort.min + GAP - MINIMUM_PRICE) / (MAXIMUM_PRICE - MINIMUM_PRICE)) * 100
+                }%`
                 setPosition(prev => ({ ...prev, right }))
-            } else if (+value > TOTAL) {
+            } else if (+value > MAXIMUM_PRICE) {
                 setPriceSort(prev => ({ ...prev, max: +value }))
-                if (maxRef.current) maxRef.current.value = TOTAL.toString()
+                if (maxRef.current) maxRef.current.value = MAXIMUM_PRICE.toString()
                 setPosition(prev => ({ ...prev, right: "0%" }))
             } else {
-                const right = `${100 - (+value / TOTAL) * 100}%`
+                const right = `${
+                    100 - ((+value - MINIMUM_PRICE) / (MAXIMUM_PRICE - MINIMUM_PRICE)) * 100
+                }%`
                 setPosition(prev => ({ ...prev, right }))
                 setPriceSort(prev => ({ ...prev, max: +value }))
                 if (maxRef.current) maxRef.current.value = value
@@ -62,9 +82,17 @@ export function PriceFilter() {
         }
     }
 
+    function onMinBlur(e: any) {
+        if (e.target.value < MINIMUM_PRICE) {
+            setPriceSort(prev => ({ ...prev, min: MINIMUM_PRICE }))
+        } else if (e.target.value > priceSort.max) {
+            setPriceSort(prev => ({ ...prev, min: prev.max - GAP }))
+        }
+    }
+
     function onMaxBlur(e: any) {
-        if (e.target.value > TOTAL) {
-            setPriceSort(prev => ({ ...prev, max: TOTAL }))
+        if (e.target.value > MAXIMUM_PRICE) {
+            setPriceSort(prev => ({ ...prev, max: MAXIMUM_PRICE }))
         } else if (e.target.value < priceSort.min) {
             setPriceSort(prev => ({ ...prev, max: prev.min + GAP }))
         }
@@ -82,6 +110,7 @@ export function PriceFilter() {
                 <Input
                     value={priceSort.min.toString()}
                     onChange={value => changeHandler(value, "min")}
+                    onBlur={onMinBlur}
                     className={styles.input}
                 />
                 <Typography variant={TypographyVariant.P} color={TypographyColor.DARK_GRAY}>
@@ -100,7 +129,8 @@ export function PriceFilter() {
                 minRef={minRef}
                 maxRef={maxRef}
                 gap={GAP}
-                total={TOTAL}
+                total={MAXIMUM_PRICE}
+                minimum={MINIMUM_PRICE}
                 left={position.left}
                 right={position.right}
             />
--- src/features/SidebarFilters/ui/PriceFilter/RangeSlider/RangeSlider.tsx
+++ src/features/SidebarFilters/ui/PriceFilter/RangeSlider/RangeSlider.tsx
@@ -7,13 +7,14 @@ interface RangeSliderProps {
     maxRef: any
     total: number
     gap: number
+    minimum: number
     left: string
     right: string
 }
 
 export function RangeSlider(props: RangeSliderProps) {
-    const { setPriceSort, minRef, maxRef, setPosition, total, left, right, gap } = props
-    console.log(maxRef.current?.value)
+    const { setPriceSort, minRef, maxRef, setPosition, total, left, right, gap, minimum } = props
+
     function changeHandler(name: string) {
         if (minRef.current && maxRef.current) {
             const min = +minRef.current.value
@@ -24,20 +25,24 @@ export function RangeSlider(props: RangeSliderProps) {
                     const min = Number(max - gap).toString()
                     minRef.current.value = min
                     setPriceSort((prev: any) => ({ ...prev, min }))
-                    const left = `${(+minRef.current.value / total) * 100}%`
-                    const right = `${100 - (+maxRef.current.value / total) * 100}%`
+                    const left = `${((+minRef.current.value - minimum) / (total - minimum)) * 100}%`
+                    const right = `${
+                        100 - ((+maxRef.current.value - minimum) / (total - minimum)) * 100
+                    }%`
                     setPosition({ left, right })
                 } else {
                     const max = Number(min + gap).toString()
                     maxRef.current.value = max
                     setPriceSort((prev: any) => ({ ...prev, max }))
-                    const left = `${(+minRef.current.value / total) * 100}%`
-                    const right = `${100 - (+maxRef.current.value / total) * 100}%`
+                    const left = `${((+minRef.current.value - minimum) / (total - minimum)) * 100}%`
+                    const right = `${
+                        100 - ((+maxRef.current.value - minimum) / (total - minimum)) * 100
+                    }%`
                     setPosition({ left, right })
                 }
             } else {
-                const left = `${(min / total) * 100}%`
-                const right = `${100 - (max / total) * 100}%`
+                const left = `${((min - minimum) / (total - minimum)) * 100}%`
+                const right = `${100 - ((max - minimum) / (total - minimum)) * 100}%`
                 setPosition({ left, right })
                 setPriceSort({ min, max })
             }
@@ -54,7 +59,7 @@ export function RangeSlider(props: RangeSliderProps) {
                     type="range"
                     ref={minRef}
                     className={styles.min}
-                    min="0"
+                    min={minimum.toString()}
                     max={total}
                     onInput={() => changeHandler("min")}
                 />
@@ -62,7 +67,7 @@ export function RangeSlider(props: RangeSliderProps) {
                     type="range"
                     ref={maxRef}
                     className={styles.max}
-                    min="0"
+                    min={minimum.toString()}
                     max={total}
                     onInput={() => changeHandler("max")}
                 />
