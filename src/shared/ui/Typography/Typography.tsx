2e3cacf6cbf9e796101da075262539b5cb86e8e7
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-22 15:35:25 +0200

    final

--- src/shared/ui/Typography/Typography.tsx
+++ src/shared/ui/Typography/Typography.tsx
@@ -5,6 +5,7 @@ import styles from "./Typography.module.scss"
 export enum TypographyColor {
     BASE = "base",
     DARK_GRAY = "darkGray",
+    INVERTED = "inverted",
 }
 
 export enum TypographyVariant {
