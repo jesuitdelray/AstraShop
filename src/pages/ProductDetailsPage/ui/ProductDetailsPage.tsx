57916d960661e090dff794ea58b4a5da9cf4a619
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-24 22:47:32 +0200

    fin

--- src/pages/ProductDetailsPage/ui/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ui/ProductDetailsPage.tsx
@@ -1,9 +1,15 @@
 import { ProductDescription } from "entities/ProductDescription"
+import { BannersRow } from "widgets/BannersRow"
+import { ProductCardList } from "widgets/ProductCarousel/model/list"
+import { ProductCarousel } from "widgets/ProductCarousel/ProductCarousel"
 
 export function ProductDetailsPage() {
     return (
         <div>
             <ProductDescription />
+            <ProductCarousel />
+            <ProductCarousel list={ProductCardList} title="Топовые позиции" />
+            <BannersRow />
         </div>
     )
 }
