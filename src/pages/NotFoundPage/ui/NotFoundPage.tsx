0539b55c5cf45f84a2da82d95d457025722f8075
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-24 22:31:05 +0200

    fin

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,3 +1,26 @@
+import { useNavigate } from "react-router-dom"
+import { RoutePath } from "shared/config/routeConfig/routeConfig"
+import { Button, ButtonVariant } from "shared/ui/Button/Button"
+import { Typography, TypographyVariant } from "shared/ui/Typography/Typography"
+import styles from "./NotFoundPage.module.scss"
+
 export function NotFoundPage() {
-    return <div>NotFoundPage</div>
+    const navigate = useNavigate()
+
+    function clickHandler() {
+        navigate(RoutePath.main)
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            <Typography variant={TypographyVariant.H2}>Страница не существует</Typography>
+            <Button
+                variant={ButtonVariant.FILLED_RED}
+                className={styles.btn}
+                onClick={clickHandler}
+            >
+                На главную
+            </Button>
+        </div>
+    )
 }
