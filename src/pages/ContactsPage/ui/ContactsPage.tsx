7871e79f038cc60a8e93204932fe2a14c276a97d
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-24 23:10:41 +0200

    fin

--- src/pages/ContactsPage/ui/ContactsPage.tsx
+++ src/pages/ContactsPage/ui/ContactsPage.tsx
@@ -1,3 +1,23 @@
+import { Contacts } from "entities/Contacts"
+import { Schedule } from "entities/Schedule"
+import { Typography, TypographyVariant } from "shared/ui/Typography/Typography"
+import { BannersRow } from "widgets/BannersRow"
+import { ProductCardList } from "widgets/ProductCarousel/model/list"
+import { ProductCarousel } from "widgets/ProductCarousel/ProductCarousel"
+import styles from "./ContactsPage.module.scss"
+
 export function ContactsPage() {
-    return <div>ContactsPage</div>
+    return (
+        <div>
+            <Typography variant={TypographyVariant.H1} className={styles.title}>
+                Контакты
+            </Typography>
+            <div className={styles.contacts}>
+                <Schedule className={styles.schedule} />
+                <Contacts className={styles.contact} />
+            </div>
+            <ProductCarousel list={ProductCardList} title="Топовые позиции" />
+            <BannersRow />
+        </div>
+    )
 }
