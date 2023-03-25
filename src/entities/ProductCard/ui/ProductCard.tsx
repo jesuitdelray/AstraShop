296afab80cafa2cb3f335ed3bd7f22ea7b7d09be
Artur <109896229+Art1us@users.noreply.github.com>
2023-03-25 10:40:49 +0200

    Revert "Revert "global/86.Link-website-together""

--- src/entities/ProductCard/ui/ProductCard.tsx
+++ src/entities/ProductCard/ui/ProductCard.tsx
@@ -2,6 +2,8 @@ import { ReactElement } from "react"
 import { Label } from "shared/ui/Label/Label"
 import { Typography } from "shared/ui/Typography/Typography"
 import { classNames } from "shared/lib/classNames/classNames"
+import { useNavigate } from "react-router-dom"
+import { RoutePath } from "shared/config/routeConfig/routeConfig"
 import styles from "./ProductCard.module.scss"
 
 export interface ProductCardProps {
@@ -17,10 +19,12 @@ export interface ProductCardProps {
 export const ProductCard = (props: ProductCardProps): ReactElement => {
     const { isNew, image, className, id, description, price, currency } = props
 
+    const navigate = useNavigate()
+
     return (
         <div
             className={classNames(styles.container, {}, [className])}
-            onClick={() => console.log(id)}
+            onClick={() => navigate(RoutePath.product_details)}
         >
             <div className={styles.header}>
                 {!!isNew && <Label value="новый" className={styles.label} />}
