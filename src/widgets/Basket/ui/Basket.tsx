384911f5b55d9422538c32087b1194bc1d899117
VadimGalian <vadim.galian@gmail.com>
2023-03-27 16:23:29 +0300

    aaa

--- src/widgets/Basket/ui/Basket.tsx
+++ src/widgets/Basket/ui/Basket.tsx
@@ -2,10 +2,7 @@ import { OrderInfo } from "entities/OrderInfo"
 import { useNavigate } from "react-router-dom"
 import { CrossIcon } from "shared/assets/icons/others"
 import { RoutePath } from "shared/config/routeConfig/routeConfig"
-import {
-    ModalSlider,
-    ModalSliderVariant,
-} from "shared/ui/ModalSlider/ModalSlider"
+import { ModalSlider, ModalSliderVariant } from "shared/ui/ModalSlider/ModalSlider"
 import { Typography, TypographyVariant } from "shared/ui/Typography/Typography"
 import { basketItemsList } from "../model/list"
 import styles from "./Basket.module.scss"
@@ -27,18 +24,13 @@ export function Basket({ isOpen, onClose }: BasketProps) {
         <ModalSlider
             isOpen={isOpen}
             onClose={onClose}
-            variant={
-                isSlideTop ? ModalSliderVariant.TOP : ModalSliderVariant.RIGHT
-            }
+            variant={isSlideTop ? ModalSliderVariant.TOP : ModalSliderVariant.RIGHT}
             className={styles.wrapper}
             containerHeight={isSlideTop ? `${height - 64}px` : "auto"}
         >
             <div className={styles.container}>
                 <div className={styles.slideRightHeader}>
-                    <Typography
-                        variant={TypographyVariant.H3}
-                        className={styles.slideRightTitle}
-                    >
+                    <Typography variant={TypographyVariant.H3} className={styles.slideRightTitle}>
                         Корзина
                     </Typography>
                     <CrossIcon onClick={onClose} className={styles.cross} />
