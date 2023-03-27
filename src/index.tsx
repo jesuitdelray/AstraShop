ebb78e08fa649468164870055814e0ce9a0b4df1
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-27 12:10:44 +0300

    fin

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import { render } from "react-dom"
 import { BrowserRouter } from "react-router-dom"
+import { StoreProvider } from "app/providers/StoreProvider"
 import App from "./app/App"
 import "app/styles/index.scss"
 import { ErrorBoundary } from "./app/providers/ErrorBoundary"
 
 render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <App />
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <App />
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
     document.getElementById("root")
 )
