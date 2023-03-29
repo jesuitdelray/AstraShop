67bc18ce5194348b17e764a1098e524b210f94ad
je-delray <hovax74935@ekcsoft.com>
2023-03-29 16:24:07 +0200

    3rd

--- src/pages/CategoryPage/ui/CategoryPage.tsx
+++ src/pages/CategoryPage/ui/CategoryPage.tsx
@@ -1,10 +1,14 @@
+import { Breadcrumbs } from "entities/Breadcrumbs"
 import { LinksList } from "entities/LinksList"
 import { catalogList } from "pages/CatalogPage/model/lists"
+import { AppRoutes } from "shared/config/routeConfig/routeConfig"
 import styles from "./CategoryPage.module.scss"
 
 export function CategoryPage() {
+    const breadcrumbsList = [AppRoutes.CATALOG]
     return (
         <div className={styles.wrapper}>
+            <Breadcrumbs breadcrumbsList={breadcrumbsList} />
             <LinksList data={catalogList[0]} />
         </div>
     )
