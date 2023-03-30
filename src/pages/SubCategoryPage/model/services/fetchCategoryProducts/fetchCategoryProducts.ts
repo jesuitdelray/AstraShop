4fa0184c1ac8d48a58f2725feee48cac43175309
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-30 14:49:42 +0300

    more fixes

--- src/pages/SubCategoryPage/model/services/fetchCategoryProducts/fetchCategoryProducts.ts
+++ src/pages/SubCategoryPage/model/services/fetchCategoryProducts/fetchCategoryProducts.ts
@@ -1,21 +1,23 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 import { ThunkConfig } from "app/providers/StoreProvider"
+import { SubCategoryPageSchema } from "../../types/subcategoryPageSchema"
 
-export const fetchCategoryProducts = createAsyncThunk<any, string | undefined, ThunkConfig<string>>(
-    "subcategoryPage/fetchCategoryProducts",
-    async (id, thunkApi) => {
-        const { extra, rejectWithValue } = thunkApi
+export const fetchCategoryProducts = createAsyncThunk<
+    SubCategoryPageSchema,
+    string | undefined,
+    ThunkConfig<string>
+>("subcategoryPage/fetchCategoryProducts", async (id, thunkApi) => {
+    const { extra, rejectWithValue } = thunkApi
 
-        try {
-            const response = await extra.api.get(`category/${id}/products`)
+    try {
+        const response = await extra.api.get(`category/${id}/products`)
 
-            if (!response.data) {
-                throw new Error()
-            }
-
-            return response.data
-        } catch (error) {
-            return rejectWithValue("error")
+        if (!response.data) {
+            throw new Error()
         }
+
+        return response.data
+    } catch (error) {
+        return rejectWithValue("error")
     }
-)
+})
