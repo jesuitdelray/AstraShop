5c339b85f3ef63d08b2a822fa62a60caa289a2b5
VadimGalian <vadim.galian@gmail.com>
2023-04-01 10:45:18 +0300

    Update AppLink.test.tsx

--- src/shared/ui/AppLink/AppLink.test.tsx
+++ src/shared/ui/AppLink/AppLink.test.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react"
 import { AppRouter } from "app/providers/router"
-import { MemoryRouter } from "react-router-dom"
+import { MemoryRouter, Route, Router, Routes } from "react-router-dom"
 import { AppLink } from "./AppLink"
 
 describe("Applink", () => {
@@ -13,13 +13,47 @@ describe("Applink", () => {
         expect(screen.getByText("Applink test")).toBeInTheDocument()
     })
     test("Applink test on click", () => {
+        const onClick = jest.fn()
         render(
             <MemoryRouter>
-                <AppLink to="/about">Applink test</AppLink>
-                <AppRouter />
+                <AppLink to="/about" onClick={onClick}>
+                    Applink test
+                </AppLink>
+                <Routes>
+                    <Route path="/" element={<></>} />
+                    <Route path="/about" element={<></>} />
+                </Routes>
             </MemoryRouter>
         )
         fireEvent.click(screen.getByText("Applink test"))
-        expect(window.location.pathname).toEqual("/about")
+        expect(onClick).toHaveBeenCalled()
     })
+})
+
+test("clicking filter links updates product query params", () => {
+    let testHistory, testLocation
+    render(
+        <MemoryRouter initialEntries={["/my/initial/route"]}>
+            <App />
+            <Route
+                path="*"
+                render={({ history, location }) => {
+                    testHistory = history
+                    testLocation = location
+                    return null
+                }}
+            />
+        </MemoryRouter>,
+        node
+    )
+
+    act(() => {
+        // example: click a <Link> to /products?id=1234
+    })
+
+    // assert about url
+    expect(testLocation.pathname).toBe("/products")
+    const searchParams = new URLSearchParams(testLocation.search)
+    expect(searchParams.has("id")).toBe(true)
+    expect(searchParams.get("id")).toEqual("1234")
 })
