3b2732f672fc9c0a3bb510df7679767c3b3132fd
Art1us <109896229+Art1us@users.noreply.github.com>
2023-04-07 16:16:27 +0300

    inter

--- src/entities/CatalogNavigation copy/index.ts
+++ src/entities/CatalogNavigation copy/index.ts
@@ -1,5 +1,5 @@
-/* export { LinksList } from "./ui/LinksList/LinksList"
-export { SidebarNavigation } from "./ui/SidebarNavigation/SidebarNavigation" */
+export { LinksList } from "./ui/LinksList/LinksList"
+export { SidebarNavigation } from "./ui/SidebarNavigation/SidebarNavigation"
 export { getNavigationTree } from "./model/selectors/sidebarNavigationSelectors"
 export { navigationTreeType } from "./model/types/list"
 export type { CatalogNavigationSchema } from "./model/types/catalogNavigationSchema"
--- src/pages/CatalogPage/ui/CatalogLinks/CatalogLinks.tsx
+++ src/pages/CatalogPage/ui/CatalogLinks/CatalogLinks.tsx
@@ -1,5 +1,7 @@
-import { LinksList, getNavigationTree, navigationTreeType } from "entities/CatalogNavigation"
+import { getNavigationTree, navigationTreeType } from "entities/CatalogNavigation"
 import { useSelector } from "react-redux"
+import { RoutePath } from "shared/config/routeConfig/routeConfig"
+import { AppLink } from "shared/ui/AppLink/AppLink"
 import styles from "./CatalogLinks.module.scss"
 
 export function CatalogLinks() {
@@ -8,8 +10,25 @@ export function CatalogLinks() {
     return (
         <div className={styles.container}>
             {navigationTree.map(item => {
-                const { id } = item
-                return <LinksList key={id} data={item} className={styles.links} />
+                const { id, name, categories } = item
+                return (
+                    <div className={styles.links}>
+                        <AppLink to={RoutePath.category + id} className={styles.title}>
+                            {name}
+                        </AppLink>
+                        <div className={styles.list}>
+                            {categories.map(item => {
+                                const { id, name } = item
+
+                                return (
+                                    <AppLink key={id} to={RoutePath.sub_category + id}>
+                                        {name}
+                                    </AppLink>
+                                )
+                            })}
+                        </div>
+                    </div>
+                )
             })}
         </div>
     )
