1cb723fe88c7535384a49cd8d230677fa2b00010
Art1us <109896229+Art1us@users.noreply.github.com>
2023-04-12 22:31:48 +0300

    fix

--- src/features/SubmitOrder/model/selectors/submitOrderSelectors.ts
+++ src/features/SubmitOrder/model/selectors/submitOrderSelectors.ts
@@ -1,5 +1,4 @@
 import { StateSchema } from "app/providers/StoreProvider"
 
-export const getSubmitOrderUrl = (state: StateSchema) => state.submitOrder.url
 export const getSubmitOrderError = (state: StateSchema) => state.submitOrder.error
 export const getSubmitOrderLoading = (state: StateSchema) => state.submitOrder.isLoading
