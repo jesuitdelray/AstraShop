50a70bf829195d94fa708fb84eb7ea8dda6f4d8e
Art1us <109896229+Art1us@users.noreply.github.com>
2023-04-12 18:06:14 +0300

    test

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -7,7 +7,7 @@ i18n.use(Backend)
     .use(initReactI18next)
     .init({
         fallbackLng: "en",
-        debug: false,
+        debug: true,
         interpolation: {
             escapeValue: false,
         },
