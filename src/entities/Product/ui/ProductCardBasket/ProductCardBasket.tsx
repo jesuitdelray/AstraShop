7240da8281be504519b8292c18fe8a7c9dc76892
VadimGalian <vadim.galian@gmail.com>
2023-04-22 17:14:56 +0300

    fin

--- src/entities/Product/ui/ProductCardBasket/ProductCardBasket.tsx
+++ src/entities/Product/ui/ProductCardBasket/ProductCardBasket.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from "react"
 import { Typography, TypographyVariant } from "shared/ui/Typography/Typography"
 import productPlaceholder from "shared/assets/images/productPlaceholder.jpg"
+import { useNavigate } from "react-router-dom"
 import styles from "./ProductCardBasket.module.scss"
 import { Product } from "../../model/types"
 
@@ -8,16 +9,18 @@ interface ProductCardBasketProps extends Product {
     Delete: ReactElement
     Counter: ReactElement
     currency: string
+    onProductClick?: (id: number) => void
 }
 
 export function ProductCardBasket(props: ProductCardBasketProps) {
-    const { images = [], name, price, Delete, Counter, currency } = props
+    const { images = [], name, price, Delete, Counter, currency, id, onProductClick } = props
     const imgSrc = images?.[0] ? images[0] : productPlaceholder
 
     return (
         <div className={styles.container}>
-            <img src={imgSrc} alt="" className={styles.img} />
-            <div className={styles.info}>
+            {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+            <img src={imgSrc} alt="" className={styles.img} onClick={() => onProductClick?.(id)} />
+            <div className={styles.info} onClick={() => onProductClick?.(id)}>
                 <Typography variant={TypographyVariant.P}>{name}</Typography>
                 <Typography variant={TypographyVariant.H3} isBold>
                     {`${price} ${currency}`}
