b1ce8fe929fb38cd58bc630f85ae3e40574928cf
Art1us <109896229+Art1us@users.noreply.github.com>
2023-04-24 15:54:58 +0300

    fin

--- src/features/basketFeatures/RemoveProductFromBasket/RemoveProductFromBasket.tsx
+++ src/features/basketFeatures/RemoveProductFromBasket/RemoveProductFromBasket.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react"
 import { basketActions } from "entities/Basket"
 import { useDispatch } from "react-redux"
-import { DeleteBucketIcon } from "shared/assets/icons/others"
+import { CrossRIcon, DeleteBucketIcon } from "shared/assets/icons/others"
+import { ModalSlider, ModalSliderVariant } from "entities/ModalSlider"
+import { Button, ButtonVariant } from "shared/ui/Button/Button"
+import { Typography, TypographyVariant } from "shared/ui/Typography/Typography"
 import styles from "./RemoveProductFromBasket.module.scss"
 
 interface RemoveProductFromBasketProps {
@@ -8,11 +12,45 @@ interface RemoveProductFromBasketProps {
 }
 
 export function RemoveProductFromBasket({ id }: RemoveProductFromBasketProps) {
+    const [isOpen, setIsOpen] = useState(false)
     const dispatch = useDispatch()
 
     function clickHandler() {
+        setIsOpen(true)
+    }
+
+    function confirmClickHandler() {
         dispatch(basketActions.removeFromBasket(id))
     }
 
-    return <DeleteBucketIcon onClick={clickHandler} className={styles.delete} />
+    function closeModalHandler() {
+        setIsOpen(false)
+    }
+
+    return (
+        <>
+            <ModalSlider
+                isOpen={isOpen}
+                onClose={closeModalHandler}
+                variant={ModalSliderVariant.MODAL}
+                className={styles.modal}
+            >
+                <div className={styles.modalContainer}>
+                    <CrossRIcon className={styles.cross} onClick={closeModalHandler} />
+                    <Typography variant={TypographyVariant.H3} className={styles.title}>
+                        Are you sure?
+                    </Typography>
+                    <div className={styles.buttons}>
+                        <Button onClick={confirmClickHandler} variant={ButtonVariant.FILLED_RED}>
+                            Delete
+                        </Button>
+                        <button className={styles.btnCancel} onClick={closeModalHandler}>
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </ModalSlider>
+            <DeleteBucketIcon onClick={clickHandler} className={styles.delete} />
+        </>
+    )
 }
