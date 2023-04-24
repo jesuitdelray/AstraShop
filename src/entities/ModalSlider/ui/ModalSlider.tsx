54725c5ea80a7e2f681f4fd6ca048333308f0672
Art1us <109896229+Art1us@users.noreply.github.com>
2023-04-24 18:55:28 +0300

    fin

--- src/entities/ModalSlider/ui/ModalSlider.tsx
+++ src/entities/ModalSlider/ui/ModalSlider.tsx
@@ -1,6 +1,6 @@
 import { ReactNode, useEffect, useRef } from "react"
 import { classNames } from "shared/lib/classNames/classNames"
-import { Modal } from "shared/ui/Modal/Modal"
+import { Modal } from "shared/ui/Modal"
 import styles from "./ModalSlider.module.scss"
 
 export enum ModalSliderVariant {
