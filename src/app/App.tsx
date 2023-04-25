d7bfec9cbac0bcfa7a8e3a0864c5913780d018ba
Art1us <109896229+Art1us@users.noreply.github.com>
2023-04-25 11:57:06 +0300

    fix linter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -18,7 +18,7 @@ function App() {
     useEffect(() => {
         dispatch(basketActions.initBasketData())
         dispatch(changeLanguageActions.initLanguage())
-    }, [])
+    }, [dispatch])
 
     const isSubCategoryPage = useMatch(`${RoutePath.sub_category}/:id`)
 
