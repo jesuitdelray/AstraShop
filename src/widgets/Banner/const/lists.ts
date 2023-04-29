1074387d4b491d99659d5719062c774f3b7827b4
je-delray <hovax74935@ekcsoft.com>
2023-04-29 12:09:33 +0200

    changed banner photos

--- src/widgets/Banner/const/lists.ts
+++ src/widgets/Banner/const/lists.ts
@@ -2,6 +2,7 @@ import { RoutePath } from "shared/config/routeConfig/const"
 import img1 from "./image31.jpg"
 import img2 from "./image32.jpg"
 import img3 from "./image33.jpg"
+import img4 from "./image34.jpg"
 
 import single1 from "./single1.jpg"
 import single2 from "./single2.jpg"
@@ -11,6 +12,7 @@ export const bannerSliderList = [
     { id: 1, link: `${RoutePath.category}/87`, img: img1 },
     { id: 2, link: `${RoutePath.category}/34`, img: img2 },
     { id: 3, link: `${RoutePath.category}/93`, img: img3 },
+    { id: 4, link: `${RoutePath.category}/100`, img: img4 },
 ]
 
 export const singleBannerList = [
