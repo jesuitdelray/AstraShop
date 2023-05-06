60b8ed99407bc5254d0e8c886c1a65d135035327
Art1us <109896229+Art1us@users.noreply.github.com>
2023-05-06 15:54:04 +0300

    fin

--- src/entities/Product/index.ts
+++ src/entities/Product/index.ts
@@ -2,7 +2,6 @@ export { ProductCard } from "./ui/ProductCard/ProductCard"
 export { ProductCardSale } from "./ui/ProductCardSale/ProductCardSale"
 export { ProductCardSaleSkeleton } from "./ui/ProductCardSale/ProductCardSaleSkeleton/ProductCardSaleSkeleton"
 export { ProductCardSkeleton } from "./ui/ProductCard/ProductCardSkeleton/ProductCardSkeleton"
-export { ProductDetails } from "./ui/ProductDetails/ProductDetails"
 export { ProductCardBasket } from "./ui/ProductCardBasket/ProductCardBasket"
 
 export type { Product, ProductAttribute } from "./model/types"
